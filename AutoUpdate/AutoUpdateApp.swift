667d5b3c0c89d168909e760a0e507bd14e3d3332
GSemix <63607553+GSemix@users.noreply.github.com>
2022-09-14 18:18:34 +0300

    Initial Commit

---
+++ AutoUpdate/AutoUpdateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoUpdateApp.swift
+//  AutoUpdate
+//
+//  Created by Семен Безгин on 14.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct AutoUpdateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
